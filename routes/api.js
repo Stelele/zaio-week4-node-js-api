ee39127fc1409a4cab55b2b5fbc0c62005ee90f0
Gift Mugweni <giftmugweni@gmail.com>
2019-12-20 09:10:07 +0200

    finished property parsing

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,8 @@
 const router = require('express').Router()
+const Property = require('../models/property')
+const bodyParser = require('body-parser')
+
+const urlParser = bodyParser.urlencoded({extended:false})
 
 const authCheck = (req, res, next) => {
     if(!req.user){
@@ -11,23 +15,52 @@ const authCheck = (req, res, next) => {
 }
 
 router.post('/property', authCheck, (req, res, next) => {
-    res.send('now adding property')
+    var property = new Property(req.body).save((err, newProperty) => {
+        if(err) res.send(err)
+
+        res.send(newProperty)
+    })
 })
 
-router.put('/property/:id', authCheck,(req, res, next) => {
-    res.send('now updating property ' + req.params.id)  
+router.put('/property/:id',authCheck,urlParser,(req, res, next) => {
+    console.log("in put")
+    var updateProp = Property.findByIdAndUpdate(req.params.id, req.body, (err, updatedDoc) => {
+        if(err) res.send(err)
+        var prop = Property.findById(updatedDoc.id, (err, got) => {
+            if(err) res.send(err)
+
+            res.send(got)
+
+        })
+
+    })
+    
 })
 
-router.delete('/property/:id', authCheck, (req, res, next) => {
-    res.send("now deleting property" + req.params.id)
+router.delete('/property/:id',authCheck,  (req, res, next) => {
+    var DeletedProperty = Property.findByIdAndDelete(req.params.id, (err, deletedProp) => {
+        if(err) res.send(err)
+
+        res.send(deletedProp.name + " was sucessfully deleted")
+
+    })
+
 })
 
-router.get("/property", authCheck, (req, res, next) => {
-    res.send("getting all properties")
+router.get("/property",authCheck,  (req, res, next) => {
+    var allPropertis = Property.find({}, (err,allProp) => {
+        if(err) res.send(err)
+
+        res.send(allProp)
+    })
 })
 
-router.get("/property/:id", authCheck, (req, res, next) => {
-    res.send("getting property "+ req.params.id)
+router.get("/property/:id",authCheck,  (req, res, next) => {
+    var uniqueProperty = Property.findById(req.params.id, (err, property) => {
+        if(err) res.send(err)
+
+        res.send(property)
+    })
 })
 
 module.exports = router
